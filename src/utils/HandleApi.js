eb2906f989991862cdd9dcd935a7aeed689bfc05
Muhammad Yasir Ghaffar <122939183+M-YasirGhaffar@users.noreply.github.com>
2024-02-07 23:58:28 +0000

    ready for deploymeny

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:8000';
+const baseUrl = 'https://todolist-restapi.onrender.com';
 
 const getAllToDo = async (setToDo) => {
     try {
